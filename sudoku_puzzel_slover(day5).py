d4a26d3193a08e876df131c88e59d4f51618dd65
ARstyles <Anandraj184507@gmail.com>
2024-06-27 00:00:57 +0530

    day 5

--- sudoku_puzzel_slover(day5).py
+++ sudoku_puzzel_slover(day5).py
@@ -1,2 +1,11 @@
+def find_next_empty(puzzel):
+    #finds the next row,col on the puzzel that's not filled yet -> -1
+    #return row,col tuple (or (none,none) if there is none)
+    for r in range(9):
+        for c in range(9):
+            if puzzel[r][c] == -1:
+                return r,c
+    return None,None #if no space in the puzzel are empty(-1)
+
 def slove_sudoku(puzzel):
-    accc
+    row,col = find_next_empty(puzzel)  #to choose where on the puzzel to make a guess
